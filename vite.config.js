607c2fee9a2ae6eab01f9acd1c25a706aae6c55b
Anastasiya <knazevanasta394@gmai.com>
2025-03-02 23:00:28 +0100

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,14 @@ export default defineConfig({
             include: ['jquery', 'slick-carousel'],
         },
     build: {
+        outDir: 'dist', // Output directory
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                about: resolve(__dirname, 'catalog.html'),
-                blog: resolve(__dirname, 'blog.html'),
-            },
+                index: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'src/pages/about.html'),
+                blog: resolve(__dirname, 'src/pages/blog.html'),
+                catalog: resolve(__dirname, 'src/pages/catalog.html'),
+            }
         },
     }
 })
