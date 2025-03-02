1df7e2df125b651e5cabf5dc910e0aa78d8bb1b7
Anastasiya <knazevanasta394@gmai.com>
2025-03-02 23:11:17 +0100

    fix base in config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+    base:  '/ceramic_studio',
         optimizeDeps: {
             include: ['jquery', 'slick-carousel'],
         },
